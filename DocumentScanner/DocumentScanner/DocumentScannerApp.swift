ce81938832495664cea36f1e5316568528a272ef
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-17 15:29:19 +0100

    Create project

---
+++ DocumentScanner/DocumentScanner/DocumentScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentScannerApp.swift
+//  DocumentScanner
+//
+//  Created by Jesus Antonio Gil on 17/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DocumentScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
